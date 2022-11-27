6f6ebbdc19d52a6c037926bf11fd9c4d08ac7dc5
Robert S <112721454+roberts-fullstack@users.noreply.github.com>
2022-11-27 17:10:52 -0500

    updated files

--- main.js
+++ main.js
@@ -2,13 +2,73 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(firstName, lastName ){
+    return {
+        firstName: firstName,
+        lastName: lastName
+    }
+}
 
+function setAge(user, age){
+    user.age = age;
+    return user;
+}
 
+function incrementAge(user){
+    user.age += 1;
+    return user;
 
+}
 
+function fixCar(car){
+    car.needsMaintenance = false;
+    return car;
+}
 
+const student = {
+    name: 'Anthony DeRosa',
+    email: 'anthony.derosa@codeimmersives.com',
+    grades: [80, 100, 95]
+};
 
+const newGrades = [88, 70, 90];
 
+function addGrades(student, newGrades){
+    student.grades = student.grades.concat(newGrades);
+    return student;
+}
+
+function getDataType(obj, key){
+    return typeof obj[key];
+
+}
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo)
+    return todos;
+}
+
+function addSong(playlist, song){
+    playlist.duration += song.duration;
+    playlist.songs.push(song);
+    return playlist;
+}
+
+function updateReportCard(reportCard, num){
+    let totalGrades = 0;
+    reportCard.grades.push(num);
+    if (num < reportCard.lowestGrade){
+        reportCard.lowestGrade = num;
+    }
+    if (num > reportCard.highestGrade){
+        reportCard.highestGrade = num;
+    }
+    for (i of reportCard.grades){
+        totalGrades += i;
+    }
+    reportCard.averageGrade = totalGrades/reportCard.grades.length;
+    return reportCard;
+}
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
